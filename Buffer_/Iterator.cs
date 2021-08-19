21565cfec8b8a51545bcb0d5f867f01457840a26
new_user <new_user@gmail.com>
2021-08-19 21:21:00 +0300

    Добавьте файлы проекта.

---
+++ Buffer_/Iterator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BufferEnumerable
+{
+   public class Iterator<T>:IEnumerable<T>
+    {
+        ListNode<T> buffer;
+        public IEnumerator<T> GetEnumerator()
+        {
+            if(this.enumerable?.GetType() == this.buffer?.GetType())
+            {
+                return enumerable.GetEnumerator();
+            }
+            return Get();
+        }
+        private IEnumerator<T> Get()
+        {
+            bool check = false;
+            var enumerator = this.enumerable.GetEnumerator();
+            try
+            {
+                while (check = enumerator.MoveNext())
+                {
+                    var temp = enumerator.Current;
+                    buffer.Add(temp);
+                    yield return temp;
+                }
+            }
+            finally
+            {
+                ((IDisposable)enumerator).Dispose();
+                if (check==false)
+                {
+                    this.enumerable = this.buffer;
+                }
+                else
+                {
+                    this.buffer = new ListNode<T>();
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+           return this.GetEnumerator();
+        }
+
+        IEnumerable<T> enumerable;
+        public Iterator(IEnumerable<T> enumerable)
+        {
+            this.enumerable = enumerable;
+            this.buffer = new ListNode<T>();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using BufferEnumerable;
+using System;
+using System.Linq;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var iterator = new Iterator<int>(Enumerable.Range(1,10).Select(a=>a+1));
+            int count = 0;
+            foreach (var item in iterator)
+            {
+                count++;
+                if (count == 4) break;
+                Console.WriteLine($"{count})" + item);
+            }
+            Console.WriteLine(new string('-', 50));
+            foreach (var item in iterator)
+            {
+                count++;
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(new string('-', 50));
+            foreach (var item in iterator)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
